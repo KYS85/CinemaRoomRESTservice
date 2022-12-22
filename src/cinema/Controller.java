5ad2e253735cb76ec5b54b6ff80cb3fff53ca575
KYS85 <y.s.kostya@gmail.com>
2022-12-22 19:59:32 +0300

    Controller commit
    second

--- src/cinema/Controller.java
+++ src/cinema/Controller.java
@@ -15,18 +15,29 @@ public class Controller implements ErrorController {
         return cinemaRoom;
     }
 
+    /* body for POST
+    {
+    "row": 2,
+    "column": 1
+    }
+     */
     @PostMapping("/purchase")
     public ResponseEntity<?> purchaseSeat( @RequestBody Seat seat ) {
         return cinemaRoom.purchase( seat );
     }
-
+    /* body for POST
+    { 
+    "token": "0e9138a7-0b69-44b1-9bfd-ecf5b31645e9"
+    }
+     */
     @PostMapping("/return")
     public ResponseEntity<?> returnSeat( @RequestBody Ticket ticket ) {
         return cinemaRoom.refund( ticket );
     }
-    
+
+    //http://localhost:28852/stats?password=super_secret  - request with secret code
     // to pass tests reset Mapping
-    @GetMapping("/stats"). // in tests used PostMapping
+    @GetMapping("/stats") // in tests used PostMapping
     public ResponseEntity<?> returnStats( @RequestParam ( required = false) String password) {
         return cinemaRoom.stats( password );
     }
